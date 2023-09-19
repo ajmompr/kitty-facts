3f843840c3f3ab319310476070d09c7a79a55b4f
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-09-19 21:21:12 +0000

    user location & weather conditions working

--- app.rb
+++ app.rb
@@ -10,7 +10,45 @@ post("/results") do
   @user_loc = params.fetch("user_location").capitalize
   @user_loc_encoded = @user_loc.gsub(" ","+")
 
+  # Assemble google geocode API URL
   @gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + @user_loc_encoded + "&key=" + ENV.fetch("GMAPS_KEY")
+
+  # Place GET request to gmaps API URL
+  @raw_resp_gmaps = HTTP.get(@gmaps_url)
+
+  # Parse JSON response
+  @parsed_resp_gmaps = JSON.parse(@raw_resp_gmaps)
+
+  # Dig for user latitude and longitude values
+  @results_array = @parsed_resp_gmaps.fetch("results")
+  @results_hash = @results_array.fetch(0) 
+  @geometry_hash = @results_hash.fetch("geometry")
+  @location_hash = @geometry_hash.fetch("location")
+
+  # Get user latitude
+  @user_lat = @location_hash.fetch("lat")
+
+  # Get user longitude
+  @user_lng = @location_hash.fetch("lng")
+
+  # Assemble pirate weather API url
+  @pirate_url = "https://api.pirateweather.net/forecast/" + ENV.fetch("PIRATE_WEATHER_KEY") + "/" + @user_lat.to_s + "," + @user_lng.to_s
+  
+  # Plage GET request to pirate weather
+  @raw_resp_pirate = HTTP.get(@pirate_url)
+
+  # Parse response
+  @parsed_resp_pirate = JSON.parse(@raw_resp_pirate)
+
+  # Dig for temp, humidity, chance of precip, and current condition values 
+  @currently_hash = @parsed_resp_pirate.fetch("currently")
+
+  @user_temp = @currently_hash.fetch("temperature")
+  @user_humidity = @currently_hash.fetch("humidity")
+  @user_precip = @currently_hash.fetch("precipProbability")
+  @user_current_conditions = @currently_hash.fetch("summary")
+
+  
 
   erb(:user_results)
 end
